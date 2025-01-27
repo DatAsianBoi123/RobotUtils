841bade2e08a6ba257fdd1781e1ada9207788cb0
DatAsianBoi123 <zach.su158@gmail.com>
2025-01-26 21:03:52 -0700

    updated desmos link

--- src/main/java/com/datasiqn/robotutils/controlcurve/ControlCurves.java
+++ src/main/java/com/datasiqn/robotutils/controlcurve/ControlCurves.java
@@ -3,7 +3,7 @@ package com.datasiqn.robotutils.controlcurve;
 /**
  * A collection of different built-in {@link ControlCurve}s.
  * <p>
- * You can go to <a href=https://www.desmos.com/calculator/jaa4cadcpf>this Desmos graph</a> to better visualize what different control curves do.
+ * You can go to <a href=https://www.desmos.com/calculator/89a8st67yg>this Desmos graph</a> to better visualize what different control curves do.
  */
 public final class ControlCurves {
     /**
