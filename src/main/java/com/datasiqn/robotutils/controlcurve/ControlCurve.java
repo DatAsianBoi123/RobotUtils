5c7734cec109187129350f2c9e2ba24d9151f9a0
DatAsianBoi123 <zach.su158@gmail.com>
2025-01-26 21:01:34 -0700

    fixed grammatical error

--- src/main/java/com/datasiqn/robotutils/controlcurve/ControlCurve.java
+++ src/main/java/com/datasiqn/robotutils/controlcurve/ControlCurve.java
@@ -46,7 +46,7 @@ public abstract class ControlCurve {
      * Gets the raw curve based on the value.
      * <p>
      * Implementing classes should not include the dead zone, minimum value, or max power in this calculation.
-     * Note that most curves will be required to by modified based on the max power. For an example, look at the implementation of the linear {@code ControlCurve}.
+     * Note that most curves will be required to be modified based on the max power. For an example, look at the implementation of the linear {@code ControlCurve}.
      * @param value The value that the
      * @return The new value applied to the certain curve
      */
